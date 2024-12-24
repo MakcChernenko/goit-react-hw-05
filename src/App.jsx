79fb2d3da48c32d74e05ef4d47203effcbe6bc53
MakcChernenko <157323123+MakcChernenko@users.noreply.github.com>
2024-12-24 16:04:15 +0200

    added NavLink

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,39 @@
-import { useState } from "react";
+import { Routes, Route, NavLink } from "react-router-dom";
+import clsx from "clsx";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Products from "./pages/Products/Products";
+import NotFound from "./pages/NotFound/NotFound";
 import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
+const buildLinkClass = ({ isActive }) => {
+  return clsx("link", isActive && "active");
+};
+
+const App = () => {
+  console.log("hello");
 
   return (
-    <>
-      <h1>hello max</h1>
-    </>
+    <div>
+      <nav>
+        <NavLink to="/" className={buildLinkClass}>
+          Home
+        </NavLink>
+        <NavLink to="/about" className={buildLinkClass}>
+          About
+        </NavLink>
+        <NavLink to="/product" className={buildLinkClass}>
+          Product
+        </NavLink>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/product" element={<Products />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
   );
-}
+};
 
 export default App;
