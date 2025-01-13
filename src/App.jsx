579205bb0fe057a54b3ea34c8dce9075189acdcd
MakcChernenko <157323123+MakcChernenko@users.noreply.github.com>
2025-01-13 16:39:27 +0200

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,77 +1,38 @@
-import {
-  Routes,
-  Route,
-  NavLink,
-  useNavigate,
-  useSearchParams,
-} from "react-router-dom";
-import clsx from "clsx";
-import Home from "./pages/Home/Home";
-import About from "./pages/About/About";
-import Products from "./pages/Products/Products";
-import ProductDetails from "./pages/ProductDetails/ProductDetails";
-import Mission from "./pages/Mission/Mission";
-import Team from "./pages/Team/Team";
-import Reviews from "./pages/Reviews/Reviews";
-import Login from "./pages/Login/Login";
-import NotFound from "./pages/NotFound/NotFound";
-import "./App.css";
+import { lazy, Suspense } from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import Navigation from "./component/Navigation/Navigation.jsx";
 
-const buildLinkClass = ({ isActive }) => {
-  return clsx("link", isActive && "active");
-};
+const HomePage = lazy(() => import("./pages/HomePage/HomePage.jsx"));
+const MoviesPage = lazy(() => import("./pages/MoviesPage/MoviesPage.jsx"));
+const MovieDetailsPage = lazy(() =>
+  import("./pages/MovieDetailsPage/MovieDetailsPage.jsx")
+);
+const NotFoundPage = lazy(() =>
+  import("./pages/NotFoundPage/NotFoundPage.jsx")
+);
+const MovieCast = lazy(() => import("./component/MovieCast/MovieCast.jsx"));
+const MovieReviews = lazy(() =>
+  import("./component/MovieReviews/MovieReviews.jsx")
+);
 
 const App = () => {
-  console.log("hello");
-  const [searchParams] = useSearchParams();
-  const name = searchParams.get("name");
-  const color = searchParams.get("color");
-  const maxPrice = searchParams.get("maxPrice");
-  const navigate = useNavigate();
-  const handleSubmit = async (values) => {
-    // const response = await FakeAPI.login(values);
-    // if (response.success) {
-    navigate("/profile", { replace: true });
-    // }
-  };
-
   return (
     <div>
       <header>
-        <nav>
-          <NavLink to="/" className={buildLinkClass}>
-            Home
-          </NavLink>
-          <NavLink to="/about" className={buildLinkClass}>
-            About
-          </NavLink>
-          <NavLink to="/product" className={buildLinkClass}>
-            Product
-          </NavLink>
-          <NavLink to="/products/144">auto</NavLink>
-        </nav>
+        <Navigation />
       </header>
-      <main className="container-main">
-        <button type="button" onClick={handleSubmit}>
-          buttonRenderLogin
-        </button>
-        <p>
-          {`тут намагаюся отримати параметри рядка запиту, таких як ${
-            (name, color, maxPrice)
-          }`}
-        </p>
-        <Routes>
-          <Route path="/" element={<Home />}>
-            <Route path="mission" element={<Mission />} />
-            <Route path="team" element={<Team />} />
-            <Route path="reviews" element={<Reviews />} />
-          </Route>
-          <Route path="/about" element={<About />} />
-          <Route path="/product" element={<Products />} />
-          <Route path="/products/:postId" element={<ProductDetails />} />
-          <Route path="/profile" element={<Login />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+      <main>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/movies" element={<MoviesPage />} />
+            <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
+              <Route path="cast" element={<MovieCast />} />
+              <Route path="reviews" element={<MovieReviews />} />
+            </Route>
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </Suspense>
       </main>
     </div>
   );
--- src/pages/ProductDetails/ProductDetails.jsx
+++
@@ -1,8 +0,0 @@
-import { useParams } from "react-router-dom";
-
-const ProductDetails = () => {
-  const params = useParams();
-  return <div>ProductDetails = {params.postId}</div>;
-};
-
-export default ProductDetails;
